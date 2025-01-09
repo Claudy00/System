e7d131b886cd6153a5f4b87fea7dea893ca2172e
claudysonClairmeus <claudysonclairmeus@estudante.sesisenai.org.br>
2025-01-09 15:36:27 -0300

    Class Produto

---
+++ SistemaDeGerenciamentoDeProduto/src/com/loja/modelo/Produto/Produto.java
@@ -0,0 +1,74 @@
+package com.loja.modelo.Produto;
+
+import java.util.Objects;
+
+public class Produto {
+    private int ID;
+    private  String nome;
+    private double preco;
+    private  int quantidadeEstoque;
+    private String categoria ;
+
+    public Produto (String nome,double preco, int quantidadeEstoque,String categoria){
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.categoria = categoria;
+    }
+
+    // metodos Getters and Setters
+    public int getID () {
+        return ID;
+    }
+    public void setID (int ID){
+        this.ID = ID;
+    }
+
+    public String getNome (){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public  double getPreco(){
+        return preco;
+    }
+    public void setPreco(double preco){
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEstoque(){
+        return quantidadeEstoque;
+    }
+    public void setQuantidadeEstoque(int quantidadeEstoque){
+      this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getCategoria() {
+        return categoria ;
+    }
+
+    // Metodo toString
+    public String toString(){
+        return "Produto "+ "\n" +
+                "ID                = " + ID + "\n" +
+                "Nome              = " + nome + "\n" +
+                "Preco             = " + preco + "\n" +
+                "QuantitadeEstoque = " + quantidadeEstoque + "\n" +
+                "Categoria         = " + categoria ;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return ID == produto.ID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(ID);
+    }
+}
