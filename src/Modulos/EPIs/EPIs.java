61efb1d71645cc989492862e5baa9d177c156516
claudysonClairmeus <claudysonclairmeus@estudante.sesisenai.org.br>
2025-04-24 15:47:59 -0300

    Listar EPIs

--- src/Modulos/EPIs/EPIs.java
+++ src/Modulos/EPIs/EPIs.java
@@ -16,6 +16,38 @@ public class EPIs {
         this.data = data;
     }
 
+    public static ArrayList<EPIs> getListarEPIs() {
+        return listarEPIs;
+    }
+
+    public static void setListarEPIs(ArrayList<EPIs> listarEPIs) {
+        EPIs.listarEPIs = listarEPIs;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
     public static void MenuEPIs(){
         Scanner entrada = new Scanner(System.in);
         int opcoe;
@@ -32,6 +64,11 @@ public class EPIs {
 
           switch (opcoe){
               case 1 : cadastrarEPIs();
+                  System.out.println();
+              break;
+              case 2 : ListarEPIs();
+                  System.out.println();
+              break;
           }
 
          }while (opcoe!=0);
@@ -46,7 +83,28 @@ public class EPIs {
         Date dataCadastrado = new Date();
         EPIs novoEPI = new EPIs(nome,quant,dataCadastrado);
         listarEPIs.add(novoEPI);
+        System.out.println("~~~~~~EPI cadastrado com sucesso~~~~~~");
+        System.out.println("Nome      : " +nome);
+        System.out.println("Qunatidade: " +quant);
+        System.out.println("Date      : " +dataCadastrado);
+        System.out.println();
+    }
 
+    public static void ListarEPIs(){
+        if (listarEPIs.isEmpty()){
+            System.out.println("Nenhum EPI cadastrado...");
+            System.out.println();
+            return;
+        }
+        for (int i=0; i<listarEPIs.size();i++){
+            System.out.println("======= Lista De EPIs =======");
+            System.out.println((i+1)+ "-" + listarEPIs.get(i));
+            System.out.println();
+        }
+    }
 
+    @Override
+    public String toString(){
+        return "Nome: " + nome + "       ✳Quantidade: " + quantidade + "         ✳Data: " + data;
     }
 }
